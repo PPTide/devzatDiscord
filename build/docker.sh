0c076b503550f9886a84e495d3975dccdbd5402a
PPTide <gitea@pptie.de>
2026-08-03 15:22:17 +0200

    update build workflow

--- build/docker.sh
+++ build/docker.sh
@@ -1,2 +1,9 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t git.pptie.de/pptide/devzat-discord:latest .
-docker push git.pptie.de/pptide/devzat-discord:latest
+docker buildx create --driver docker-container --use --name builder || docker buildx use builder
+docker buildx inspect --bootstrap
+
+docker login git.pptie.de -u "$REGISTRY_USER" -p "$REGISTRY_PASSWORD"
+
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t git.pptie.de/pptide/devzat-discord:latest \
+  --push .
